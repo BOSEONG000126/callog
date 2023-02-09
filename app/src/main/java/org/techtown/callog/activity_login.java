5ffed9ca4323bfbbb7ef513f72055234dd8f8a9d
boseong0636 <qhtjd0632@naver.com>
2023-02-09 18:26:43 +0900

    4 commit

--- app/src/main/java/org/techtown/callog/activity_login.java
+++ app/src/main/java/org/techtown/callog/activity_login.java
@@ -1,14 +1,21 @@
 package org.techtown.callog;
 
+import static android.app.PendingIntent.getActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class activity_login extends AppCompatActivity {
 
         TextView sign;
+        Button login;
 
         @Override
         protected void onPostCreate(Bundle savedInstanceState) {
@@ -21,6 +28,13 @@ public class activity_login extends AppCompatActivity {
             //회원가입 버튼 클릭시, 회원가입 페이지로 이동
             sign.setOnClickListener(v -> {
                 Intent intent = new Intent(this, Signup.class);
+                startActivity(intent);
+            });
+            //로그인 버튼
+            login = findViewById(R.id.loginbutton);
+
+            login.setOnClickListener(v -> {
+                Intent intent = new Intent(this, MainActivity.class);
                 startActivity(intent);
             });
         }
